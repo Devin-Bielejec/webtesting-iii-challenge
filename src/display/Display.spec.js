3767699f4e0d3f89c97a3b23fc240740e1d689c5
devin-bielejec <devin.bieleje@gmail.com>
2019-09-10 12:32:06 -0400

    Testing: Display: Gate is open closed locked and unlocked complete

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -8,7 +8,7 @@ import Display from "./Display";
 import { act } from "react-testing-library";
 
 //Gate Exists
-test("Gate is unlocked and open", () => {
+test("Gate is unlocked and open by default", () => {
     const display = render(<Display />);
 
     //Check if div is unlocked
@@ -18,5 +18,29 @@ test("Gate is unlocked and open", () => {
     const gateOpen = display.findByText("Open");
 })
 
+
+test("Gate is locked if locked", () => {
+    const display = render(<Display locked={true}/>);
+    //Check to see if gate is locked
+    const gate = display.findByText("Locked");    
+})
+
+test("Gate is unlocked if unlocked", () => {
+    const display = render(<Display locked={false}/>);
+    //Check to see if gate is locked
+    const gate = display.findByText("Unlocked");    
+})
+
+test("Gate is open if open", () => {
+    const display = render(<Display closed={false}/>);
+    //Check to see if gate is locked
+    const gate = display.findByText("Open");    
+})
+
+test("Gate is closed if closed", () => {
+    const display = render(<Display closed={true}/>);
+    //Check to see if gate is locked
+    const gate = display.findByText("Closed");    
+})
 
 

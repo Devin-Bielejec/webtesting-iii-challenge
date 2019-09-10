90a3fc16af226558dc1525fd870fdd587aa82b8c
devin-bielejec <devin.bieleje@gmail.com>
2019-09-10 14:42:25 -0400

    Buttons on controls are tested and exist

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Controls from "./Controls";
+
+//Gate Exists
+test("Buttons to close and lock gate exisit", () => {
+    const controls = render(<Controls />);
+
+    //Check button Lock Gate exists
+    const lockButton = document.querySelector("div.controls:first-child");
+    expect(lockButton).toHaveTextContent("Lock Gate");
+
+    const closeButton = document.querySelector("div.controls:last-child");
+    expect(closeButton).toHaveTextContent("Close Gate");
+})
+
+test("")
+
